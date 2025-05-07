3eba1faa4c703bfc034360b33856bacb12486a09
fahminlb33 <fahminlb33@gmail.com>
2025-05-07 22:02:33 +0700

    feat: remove placeholder images

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -25,7 +25,6 @@ export const Projects: Project[] = [
 		title: "MCP Server Authorization using Permit.io",
 		categories: ["open-source", "active"],
 		tech: ["nodejs", "typescript", "mcp", "sqlite"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/devto-permitio-mcp",
 		appUrl:
 			"https://dev.to/fahminlb33/authorization-in-mcp-server-with-permitio-a-jira-like-task-management-api-492l",
@@ -47,7 +46,6 @@ export const Projects: Project[] = [
 			"jupyter",
 			"uv",
 		],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/autogbifml",
 		appUrl: "https://paus.kodesiana.app",
 		description:
@@ -68,7 +66,6 @@ export const Projects: Project[] = [
 			"openai",
 			"postgres",
 		],
-		coverImage: "https://placehold.co/600x400",
 		description:
 			"Sundanese-focused information retrieval and evaluation using LLM-generated synthetic data",
 	},
@@ -78,7 +75,6 @@ export const Projects: Project[] = [
 		title: "Ritsu-Pi",
 		categories: ["open-source", "active"],
 		tech: ["ansible", "docker", "grafana", "prometheus", "mlflow"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/ritsu-pi",
 		description:
 			"A home lab server provisioning automation using Ansible playbook and Docker containers",
@@ -89,7 +85,6 @@ export const Projects: Project[] = [
 		title: "PPP Salary Calculator",
 		categories: ["open-source", "active"],
 		tech: ["typescript", "alpinejs"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/Kodesiana/ppp-calculator",
 		appUrl: "https://kalkulator-gaji.kodesiana.app",
 		description:
@@ -101,7 +96,6 @@ export const Projects: Project[] = [
 		title: "Bogor House Price Analysis",
 		categories: ["open-source"],
 		tech: ["python", "duckdb", "powerbi", "gemini", "jupyter", "uv"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/bogor-house-price",
 		description:
 			"End-to-end house price analysis in Bogor, Indonesia. Integrated agentic chat with Gemini AI and an MCP server",
@@ -112,7 +106,6 @@ export const Projects: Project[] = [
 		title: "CloudFlare R2 Proxy",
 		categories: ["open-source", "active"],
 		tech: ["typescript", "cloudflare"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/Kodesiana/blob-proxy",
 		description:
 			"CloudFlare R2 proxy using Workers for providing Azure-like SAS token to support file sharing",
@@ -123,7 +116,6 @@ export const Projects: Project[] = [
 		title: "AuthNET",
 		categories: ["research", "active"],
 		tech: ["python", "pytorch", "scikitLearn", "xgboost", "jupyter", "uv"],
-		coverImage: "https://placehold.co/600x400",
 		description:
 			"Autoencoder-based deep learning model for chemometrics application in food authentication",
 	},
@@ -133,7 +125,6 @@ export const Projects: Project[] = [
 		title: "Pohonfit",
 		categories: ["research", "active"],
 		tech: ["typescript", "react", "numpy", "wasm", "mantine"],
-		coverImage: "https://placehold.co/600x400",
 		appUrl: "https://pohonfit.kodesiana.app",
 		description:
 			"Tool for estimating tree growth using a novel nonlinear regression model, running entirely in Python WASM",
@@ -144,7 +135,6 @@ export const Projects: Project[] = [
 		title: "Pendekin",
 		categories: ["open-source", "active"],
 		tech: ["typescript", "cloudflare"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/Kodesiana/pendekin",
 		description:
 			"URL shortening API using CloudFlare Worker and KV. Used for outbound links in Kodesiana.com",
@@ -163,7 +153,6 @@ export const Projects: Project[] = [
 			"postgres",
 			"docker",
 		],
-		coverImage: "https://placehold.co/600x400",
 		description: "Simple point-of-sales and stock management for wholesaler.",
 	},
 	{
@@ -172,7 +161,6 @@ export const Projects: Project[] = [
 		title: "KawanPaper",
 		categories: ["open-source"],
 		tech: ["nodejs", "typescript", "postgres"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/KFlearning",
 		appUrl:
 			"https://dev.to/fahminlb33/systematic-literature-review-with-kawanpaper-110l",
@@ -185,7 +173,6 @@ export const Projects: Project[] = [
 		title: "Non-invasive Hemoglobin Measurement",
 		categories: ["open-source", "research"],
 		tech: ["python", "scikitLearn", "jupyter", "uv"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/hb-noninvasive",
 		description:
 			"A novel non-invasive hemoglobin (Hb) prediction from near-infrared sensor using FFT and machine learning models",
@@ -196,7 +183,6 @@ export const Projects: Project[] = [
 		title: "Autism Diagnosis from EEG Signal",
 		categories: ["open-source", "research"],
 		tech: ["python", "tensorFlow", "jupyter", "uv"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/eegautism",
 		description:
 			"Comparison of multiple convolutional neural network architectures for diagnosing autism from 16 channels EEG",
@@ -207,7 +193,6 @@ export const Projects: Project[] = [
 		title: "PicoNvidiaUpdater",
 		categories: ["open-source"],
 		tech: ["dotnet"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/PicoNvidiaUpdater",
 		description:
 			"Small tool to update Nvidia GPU drivers without the annoying Nvidia GeForce Experience app",
@@ -218,7 +203,6 @@ export const Projects: Project[] = [
 		title: "KPU 2024 Image Analytics",
 		categories: ["open-source", "active"],
 		tech: ["python", "pandas", "scrapy", "opencv", "jupyter", "uv"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/kpu2024-pleno-readability",
 		description:
 			"Analysis of the Indonesia's Presidential Election 2024 ballot form. Detecting blur and possible manipulations",
@@ -229,7 +213,6 @@ export const Projects: Project[] = [
 		title: "KFLearning",
 		categories: ["open-source"],
 		tech: ["dotnet", "winforms"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/KFlearning",
 		description:
 			"Computer lab setup tool and student project management, tailored to help TAs in Pakuan University",
@@ -240,7 +223,6 @@ export const Projects: Project[] = [
 		title: "Voyager",
 		categories: ["open-source", "research"],
 		tech: ["python", "numpy", "jupyter", "ffmpeg", "uv"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/voyager",
 		description: "Voyager (satellite) mission record decoder to images",
 	},
@@ -260,7 +242,6 @@ export const Projects: Project[] = [
 			"docker",
 			"nginx",
 		],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/PicoNvidiaUpdater",
 		description:
 			"Soil quality sensor using ESP-32 with a full-stack website dashboard and mobile app (MAUI) for monitoring",
@@ -271,7 +252,6 @@ export const Projects: Project[] = [
 		title: "Redis Status Page",
 		categories: ["open-source"],
 		tech: ["dotnet", "aspnetcore", "redis", "docker"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/RedisStatusPage",
 		appUrl: "https://dev.to/fahminlb33/status-page-with-redis-5gh7",
 		description: "Simple status page built using Redis Stack for a hackathon",
@@ -282,7 +262,6 @@ export const Projects: Project[] = [
 		title: "Bedah Data APBD 2022",
 		categories: ["open-source"],
 		tech: ["python", "pandas", "gcp", "jupyter"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/bedah-data-apbd-2022",
 		description:
 			"Analysis of Regional Government Budget (APBD) usage compliance by district",
@@ -293,7 +272,6 @@ export const Projects: Project[] = [
 		title: "China House Price Prediction",
 		categories: ["open-source"],
 		tech: ["python", "xgboost", "scikitLearn", "jupyter"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/ieee-cyberc-2022",
 		description:
 			"IEEE CyberC Data Analytics Competition 2022. A model for predicting house prices in China regions",
@@ -304,7 +282,6 @@ export const Projects: Project[] = [
 		title: "Paddy Disease Detection",
 		categories: ["open-source", "research"],
 		tech: ["python", "tensorFlow", "jupyter", "uv"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/rice-paddy-classification",
 		description:
 			"Image classification model based on MobileNetV2 to classify 5 disease from paddy images (undergraduate thesis)",
@@ -315,7 +292,6 @@ export const Projects: Project[] = [
 		title: "NatsuPy",
 		categories: ["open-source"],
 		tech: ["python", "arduino", "numpy", "jupyter"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/natsupy",
 		description:
 			"Analysis of Regional Government Budget (APBD) usage compliance by district",
@@ -326,7 +302,6 @@ export const Projects: Project[] = [
 		title: "Soberity Prediction using EEG",
 		categories: ["open-source", "research"],
 		tech: ["python", "numpy", "scikitLearn", "tensorFlow", "jupyter"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/sibyl_eeg",
 		description:
 			"Predicting person soberity from alcohol-induced use using EEG signal",
@@ -345,7 +320,6 @@ export const Projects: Project[] = [
 			"apacheSpark",
 			"jupyter",
 		],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/ieee-gsc-2021",
 		description:
 			"Predicting server fault from log data and categorizing sentences into multiple categories",
@@ -356,7 +330,6 @@ export const Projects: Project[] = [
 		title: "COVID-19 Diagnosis using Chest X-ray Images",
 		categories: ["open-source", "research"],
 		tech: ["python", "numpy", "scikitLearn", "tensorFlow", "flask", "jupyter"],
-		coverImage: "https://placehold.co/600x400",
 		githubUrl: "https://github.com/fahminlb33/covid-19-flask",
 		description:
 			"Predicting server fault from log data and categorizing sentences into multiple categories",
