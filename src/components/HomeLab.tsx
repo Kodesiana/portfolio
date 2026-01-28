a04dd93bdf1937b066ca464e2bead88969d790c4
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2026-01-28 08:44:24 +0000

    feat: update homelab info

--- src/components/HomeLab.tsx
+++ src/components/HomeLab.tsx
@@ -1,37 +1,34 @@
-import { Badge, Flex, Image, Stack, Text } from "@mantine/core";
+import { Badge, Flex, Group, Image, SimpleGrid, Stack, Text } from "@mantine/core";
 
-import HomeLabPng from "~/assets/server-rack.jpg";
+import HomeLabPng from "~/assets/server-rack.webp";
 
 export default function HomeLabSection() {
 	return (
-		<Stack align="center" justify="center">
+		<SimpleGrid cols={2}>
 			<Image
 				src={HomeLabPng}
 				alt="Fahmi's home server"
 				radius="md"
-				w="50%"
-				h="auto"
+				w="90%"
 			/>
-			<Text ta="center">
-				I managed a small home lab based on a Raspberry Pi 4b with an external 2
-				TB WD RED hard drive.
-				<br />
-				The server is running Raspbian OS and multiple apps in Docker container,
-				managed via Ansible playbook.
-				<br />
-				The netwoking is managed through a Mikrotik hEX with firewall and QoS
-				control via Queues.
-			</Text>
+			<Stack align="center" justify="center">
+				<Text>
+					I have a small home lab with 6 TB of storage (3x2 TB WD RED), an Intel i5-10400 processor, and 32 GB of DDR4 memory.
+					<br />
+					The server is running Ubuntu Server with mergerfs for storage. Almost everything is run using Docker/Podman, and fully managed via Ansible playbook.
+					<br />
+					The netwoking gear mostly consists of Mikrotik products (hEX, hAP) and TP-LINK APs.
+				</Text>
 
-			<Text mt="md">Some of the apps running in my server:</Text>
-			<Flex align="center" justify="center" wrap="wrap" gap="sm">
-				<Badge>Grafana</Badge>
-				<Badge>Prometheus</Badge>
-				<Badge>cAdvisor</Badge>
-				<Badge>Portainer</Badge>
-				<Badge>Pi-hole</Badge>
-				<Badge>MLflow</Badge>
-			</Flex>
-		</Stack>
+				<Text mt="md">Some of the apps running in my server:</Text>
+				<Flex align="center" justify="center" wrap="wrap" gap="sm">
+					<Badge>Pi-hole</Badge>
+					<Badge>Grafana</Badge>
+					<Badge>Prometheus</Badge>
+					<Badge>MLflow</Badge>
+					<Badge>Tailscale</Badge>
+				</Flex>
+			</Stack>
+		</SimpleGrid>
 	);
 }
